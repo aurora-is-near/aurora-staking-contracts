e3b38bf2dbf6e913d021e066fc63102ce5103264
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2022-04-25 14:54:48 +0200

    fix granting roles in jet staking deployment script

--- deploy/003_deploy_jet_staking.ts
+++ deploy/003_deploy_jet_staking.ts
@@ -10,15 +10,23 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
         MIN_WEIGHT,
         MAX_WEIGHT,
         AURORA_STREAM_OWNER,
-        SCHEDULE_START_TIME
+        SCHEDULE_START_TIME,
+        TESTING,
+        AURORA_TOKEN,
+        DEFAULT_ADMIN_ROLE_ADDRESS,
+        PAUSER_ROLE_ADDRESS,
+        AIRDROP_ROLE_ADDRESS,
+        CLAIM_ROLE_ADDRESS,
+        STREAM_MANAGER_ROLE_ADDRESS
     } = process.env
 
     const { deploy } = hre.deployments
     let startTime: any
+    let aurora: any
     const { owner } = await hre.getNamedAccounts()
     SCHEDULE_START_TIME ? startTime = parseInt(SCHEDULE_START_TIME as string) :  startTime = Math.floor(Date.now()/ 1000) + 60 
     const treasury = (await hre.ethers.getContract("Treasury")).address
-    const aurora = (await hre.ethers.getContract("Token")).address
+    TESTING ? aurora = (await hre.ethers.getContract("Token")).address: aurora = AURORA_TOKEN
     const scheduleTimes = [
         startTime,
         startTime + parseInt(ONE_YEAR as string),
@@ -56,9 +64,49 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
             parseInt(MIN_WEIGHT as string)
         ]
     })
-    //TODO: transfer ownership to the admin address
-    // await jet.transferOwnership(admin)
+    if(!TESTING) {
+        const jetStakingV1 = await hre.ethers.getContract("JetStakingV1")
+        await jetStakingV1.deployed()
+        const claimRole = await jetStakingV1.CLAIM_ROLE()
+        const airdropRole = await jetStakingV1.AIRDROP_ROLE()
+        const pauseRole = await jetStakingV1.PAUSE_ROLE()
+        // const streamManagerRole = await jetStakingV1.STREAM_MANAGER_ROLE()
+        const defaultAdminRole = await jetStakingV1.DEFAULT_ADMIN_ROLE()
+        console.log(`CLAIM_ROLE: ${claimRole}`)
+        console.log(`AIRDROP_ROLE: ${airdropRole}`)
+        console.log(`PAUSE_ROLE: ${pauseRole}`)
+        console.log(`DEFAULT ADMIN ROLE: ${defaultAdminRole}`)
+        // // await jetStakingV1.connect(owner).grantRole(streamManagerRole, STREAM_MANAGER_ROLE_ADDRESS)
+        await jetStakingV1.grantRole(claimRole, CLAIM_ROLE_ADDRESS)
+        console.log(
+            'ADDRESS ', 
+            CLAIM_ROLE_ADDRESS,
+            `Has a role ${claimRole}? `,
+            await jetStakingV1.hasRole(claimRole, CLAIM_ROLE_ADDRESS)
+        )
+        await jetStakingV1.grantRole(airdropRole, AIRDROP_ROLE_ADDRESS)
+        console.log(
+            'ADDRESS ', 
+            AIRDROP_ROLE_ADDRESS,
+            `Has a role ${airdropRole}? `,
+            await jetStakingV1.hasRole(airdropRole, AIRDROP_ROLE_ADDRESS)
+        )
+        await jetStakingV1.grantRole(pauseRole, PAUSER_ROLE_ADDRESS)
+        console.log(
+            'ADDRESS ', 
+            PAUSER_ROLE_ADDRESS,
+            `Has a role ${pauseRole}? `,
+            await jetStakingV1.hasRole(pauseRole, PAUSER_ROLE_ADDRESS)
+        )
+        await jetStakingV1.transferOwnership(DEFAULT_ADMIN_ROLE_ADDRESS)
+        console.log(
+            'ADDRESS ', 
+            DEFAULT_ADMIN_ROLE_ADDRESS,
+            `Has a role ${defaultAdminRole}? `,
+            await jetStakingV1.hasRole(defaultAdminRole, DEFAULT_ADMIN_ROLE_ADDRESS)
+        )
 
+    }
 }
 
 module.exports = func
