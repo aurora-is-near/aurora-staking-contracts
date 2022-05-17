c702aa7c2f75b748eaf6565b4e7f1de4b9a3ca15
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2022-05-17 10:38:03 +0200

    add propose streams scripts

--- scripts/proposeStreamTRI.js
+++ scripts/proposeStreamTRI.js
@@ -7,12 +7,12 @@ async function main() {
     SCHEDULE_START_TIME,
     AURORA_TOKEN,
   } = process.env
-  const STREAM_AURORA_AMOUNT = hre.ethers.utils.parseUnits("100", 18)
+  const STREAM_AURORA_AMOUNT = hre.ethers.utils.parseUnits("30000", 18)
   const auroraAddress = AURORA_TOKEN ? AURORA_TOKEN : (await hre.ethers.getContract("Token")).address
   const startTime = SCHEDULE_START_TIME ? parseInt(SCHEDULE_START_TIME) : Math.floor(Date.now()/ 1000) + 60
-  const STREAM_TOKEN_ADDRESS = ""
+  const STREAM_TOKEN_ADDRESS = "0xFa94348467f64D5A457F75F8bc40495D33c65aBB"
   const STREAM_TOKEN_DECIMALS = 18
-  const STREAM_OWNER = ""
+  const STREAM_OWNER = "0x1232726DA91B25D22239C5707FE85E8F078F3532"
   const scheduleTimes = [
     startTime,
     startTime + parseInt(SCHEDULE_PERIOD),
@@ -21,15 +21,15 @@ async function main() {
     startTime + 4 * parseInt(SCHEDULE_PERIOD)
   ]
   const scheduleRewards = [
-    hre.ethers.utils.parseUnits("6000000", STREAM_TOKEN_DECIMALS),// 900k
-    hre.ethers.utils.parseUnits("5100000", STREAM_TOKEN_DECIMALS), // 1.2M
-    hre.ethers.utils.parseUnits("3900000", STREAM_TOKEN_DECIMALS), // 1.8M
-    hre.ethers.utils.parseUnits("2100000", STREAM_TOKEN_DECIMALS), // 2.1M
+    hre.ethers.utils.parseUnits("1000000", STREAM_TOKEN_DECIMALS), // 100%
+    hre.ethers.utils.parseUnits("850000", STREAM_TOKEN_DECIMALS), // 85%
+    hre.ethers.utils.parseUnits("650000", STREAM_TOKEN_DECIMALS), // 65%
+    hre.ethers.utils.parseUnits("350000", STREAM_TOKEN_DECIMALS), // 35%
     // Last amount should be 0 so scheduleTimes[4] marks the end of the stream schedule.
     hre.ethers.utils.parseUnits("0", STREAM_TOKEN_DECIMALS), // 0M
   ]
   const MAX_DEPOSIT_AMOUNT = scheduleRewards[0]
-  const MIN_DEPOSIT_AMOUNT = scheduleRewards[0] // or something less
+  const MIN_DEPOSIT_AMOUNT = scheduleRewards[0].div(2) // or something less
 
   const [ streamManager ] = await hre.ethers.getSigners()
 
