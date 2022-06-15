52a0ea7dfc7150442f0c0598b60e5d519c9fba8f
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2022-06-15 12:35:42 +0200

    add propose and create vote stream

---
+++ scripts/proposeStreamVOTE.js
@@ -0,0 +1,69 @@
+const hre = require("hardhat");
+
+async function main() {
+  const {
+    SCHEDULE_PERIOD,
+    TAU_PER_STREAM,
+    SCHEDULE_START_TIME,
+  } = process.env
+  const STREAM_AURORA_AMOUNT = hre.ethers.utils.parseUnits("0", 18) // zero arora reward for the VOTE stream
+  const startTime = SCHEDULE_START_TIME ? parseInt(SCHEDULE_START_TIME) : Math.floor(Date.now()/ 1000) + 60
+  const STREAM_TOKEN_ADDRESS = "" // update me
+  const STREAM_TOKEN_DECIMALS = 18
+  const STREAM_OWNER = "" // update me
+  const scheduleTimes = [
+    startTime,
+    startTime + parseInt(SCHEDULE_PERIOD)
+  ]
+  const scheduleRewards = [ // update me
+    hre.ethers.utils.parseUnits("1000000000", STREAM_TOKEN_DECIMALS), // 100%
+    hre.ethers.utils.parseUnits("500000000", STREAM_TOKEN_DECIMALS), // 50%
+    // Last amount should be 0 so scheduleTimes[4] marks the end of the stream schedule.
+    hre.ethers.utils.parseUnits("0", STREAM_TOKEN_DECIMALS), // 0M
+  ]
+
+  const MAX_DEPOSIT_AMOUNT = scheduleRewards[0]
+  const MIN_DEPOSIT_AMOUNT = scheduleRewards[0].div(2) // or something less
+
+  const [ streamManager ] = await hre.ethers.getSigners()
+
+  const jetStakingV1 = await hre.ethers.getContract("JetStakingV1")
+  const streamManagerRole = await jetStakingV1.STREAM_MANAGER_ROLE()
+  if(!await jetStakingV1.hasRole(streamManagerRole, streamManager.address)) {
+    throw new Error(`Signer '${streamManager.address}' doesn't have STREAM_MANAGER_ROLE`)
+  }
+
+  // ^^^^^ TODO: Edit above parameters ^^^^^
+  // =======================================
+
+  // FOR this stream we don't have approval for AURORA rewards
+
+  // const auroraToken = new hre.ethers.Contract(
+  //   auroraAddress,
+  //   ["function approve(address spender, uint value)"],
+  //   streamManager
+  // )
+  // const approvalTx = await auroraToken.approve(jetStakingV1.address, STREAM_AURORA_AMOUNT)
+  // console.log("Approving AURORA: ", approvalTx.hash)
+  // await approvalTx.wait()
+
+  const proposalTx = await jetStakingV1.proposeStream(
+    STREAM_OWNER,
+    STREAM_TOKEN_ADDRESS,
+    STREAM_AURORA_AMOUNT,
+    MAX_DEPOSIT_AMOUNT,
+    MIN_DEPOSIT_AMOUNT,
+    scheduleTimes,
+    scheduleRewards,
+    parseInt(TAU_PER_STREAM),
+  )
+  console.log("Proposing stream: ", proposalTx.hash)
+  await proposalTx.wait()
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
