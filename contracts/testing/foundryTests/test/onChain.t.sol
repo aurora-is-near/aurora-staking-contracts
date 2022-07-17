7fcd97558897e19ac85d8850e3699b0f5f8eab24
lempire123 <61431140+lempire123@users.noreply.github.com>
2022-07-17 18:25:04 +0200

    update

--- contracts/testing/foundryTests/test/onChain.t.sol
+++ contracts/testing/foundryTests/test/onChain.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
-import "../src/JetStakingV1.sol";
+import ".../contracts/JetStakingV1.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 import "forge-std/Test.sol";
