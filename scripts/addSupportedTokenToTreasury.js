d1af31380d7c6401fa2c430cb059b9d9cf86fae8
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2022-07-22 10:08:13 +0200

    fix: use the treasury ABI directly

--- scripts/addSupportedTokenToTreasury.js
+++ scripts/addSupportedTokenToTreasury.js
@@ -1,10 +1,15 @@
 const hre = require("hardhat");
 
 async function main() {
-  const streamToken = "0x7a5D955B6526a559C2fB85c322451dB79DcccEC8" // update me
-  const [ streamManager ] = await hre.ethers.getSigners()
-  const treasury = await hre.ethers.getContract("Treasury")
-  const tx = await treasury.connect(streamManager).addSupportedToken(streamToken)
+  const streamToken = "0x6edE987A51d7b4d3945E7a76Af59Ff2b968910A8" // update me
+  const [ treasuryManager ] = await hre.ethers.getSigners() // make sure you are using the private key for
+  const treasuryAddress = "0xF075c896CbbB625E7911E284cD23EE19bdCCf299"
+  const treasury = new hre.ethers.Contract(
+    treasuryAddress,
+    ["function addSupportedToken(address _token)"],
+    treasuryManager
+  )
+  const tx = await treasury.addSupportedToken(streamToken)
   console.log("Adding supported Token to treasury: ", tx.hash)
   await tx.wait()
   console.log(
