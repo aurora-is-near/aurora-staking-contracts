2184b700645a701561be0fbe05004f52b82e3056
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2022-08-09 15:46:49 +0200

    fix: staking factory testing

--- test/unit/StakingStrategyFactory.test.ts
+++ test/unit/StakingStrategyFactory.test.ts
@@ -5,14 +5,7 @@ import { getEventLogs } from "./testHelper";
 
 describe("LockedStaking", function () {
     let auroraOwner: any
-    let stakingAdmin: any
     let user1: any
-    let user2: any
-    let user3: any
-    let user4: any
-    let user5: any
-    let spender: any
-    let streamOwner: any
     let oneDay: any
     let stakingFactory: any
     let auroraToken: any
@@ -22,20 +15,12 @@ describe("LockedStaking", function () {
     
     before(async () => {
         // deploys all the contracts
-        [auroraOwner, stakingAdmin, user1, user2, user3, user4, user5, spender, streamOwner] = await ethers.getSigners()
+        [auroraOwner, user1] = await ethers.getSigners()
         jetStakingV1 = await ethers.getContract("JetStakingV1")
         LockedStakingTemplate =  await ethers.getContractFactory('LockedStakingSubAccountImplementation')
         oneDay = 86400
         startTime = (await ethers.provider.getBlock("latest")).timestamp + 10
         auroraToken = await ethers.getContract("Token")
-        // const amount = ethers.utils.parseUnits("1", 18) // 1 AURORA
-        // const extraInitParameters = ethers.utils.defaultAbiCoder.encode(
-        //     ["address", "uint256"], 
-        //     [
-        //         auroraToken.address,
-        //         oneDay
-        //     ]
-        // )
 
         // Deploy Locked Staking Template
         const lockedStakingTemplate = await LockedStakingTemplate.deploy()
@@ -59,25 +44,18 @@ describe("LockedStaking", function () {
 
     it('should allow anyone to deploy a new locked staking instance', async () => {
         const amount = ethers.utils.parseUnits("1", 18)
+        const voteToken = auroraToken // random vote token
         const extraInitParameters = ethers.utils.defaultAbiCoder.encode(
             ["address", "uint256"], 
             [
-                auroraToken.address,
+                voteToken.address,
                 oneDay
             ]
-        )
-        console.log(
-            await auroraToken.balanceOf(auroraOwner.address),
-            await auroraToken.allowance(auroraOwner.address, stakingFactory.address)
         )
         await auroraToken.connect(auroraOwner).approve(
             stakingFactory.address,
             amount
         )
-        console.log(
-            await auroraToken.allowance(auroraOwner.address, stakingFactory.address)
-        )
-
         const tx = await stakingFactory.connect(auroraOwner).cloneTemplate(
             0, // template id
             amount,
@@ -89,5 +67,34 @@ describe("LockedStaking", function () {
         expect(
             await lockedStakingSubAccount.stakingContract()
         ).to.eq(jetStakingV1.address)
+        expect(
+            await stakingFactory.clonesCount()
+        ).to.be.eq(1)
+        const templatesCount = await stakingFactory.getTemplatesCount()
+        expect(
+            await stakingFactory.getUserClones(owner)
+        ).to.deep.eq(
+            [
+                [
+                    ethers.BigNumber.from(templatesCount.toNumber() - 1),
+                    instance
+                ]
+            ]
+        )
+    })
+    it('should only default admin role add new templates', async () => {
+        const NewTemplate =  await ethers.getContractFactory('LockedStakingSubAccountImplementation')
+        // Deploy the template
+        const templateId = await stakingFactory.getTemplatesCount()
+        const newTemplate = await NewTemplate.deploy()
+        await stakingFactory.connect(auroraOwner).addTemplate(newTemplate.address)
+        expect(
+            newTemplate.address
+        ).be.eq(
+            await stakingFactory.templates(templateId)
+        )
+        await expect(
+            stakingFactory.connect(user1).addTemplate(newTemplate.address)
+        ).to.be.reverted
     })
 })
