b492f0f946e61dbd3b81c8132bab848934d3316a
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2022-08-11 21:25:37 +0200

    style: minor deployment script cleanup

--- deploy/003_deploy_locked_staking.ts
+++ deploy/003_deploy_locked_staking.ts
@@ -4,30 +4,17 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     const {
-        AURORA_TOKEN,
-        // SCHEDULE_START_TIME,
-        // SCHEDULE_PERIOD
+        AURORA_TOKEN
     } = process.env
 
     const auroraTokenAddress = AURORA_TOKEN? AURORA_TOKEN : (await hre.ethers.getContract("Token")).address
-    // const startTime = SCHEDULE_START_TIME ? parseInt(SCHEDULE_START_TIME as string) : Math.floor(Date.now()/ 1000) + 60
-    // const [ deployer ] = await hre.ethers.getSigners()
-    // const treasury = await hre.ethers.getContract("Treasury")
     const jetStakingV1 = await hre.ethers.getContract("JetStakingV1")
     const LockedStakingTemplate =  await ethers.getContractFactory('LockedStakingSubAccountImplementation')
-    // const amount = ethers.utils.parseUnits("1", 18) // 1 AURORA
-    // const extraInitParameters = ethers.utils.defaultAbiCoder.encode(
-    //     ["address", "uint256"], 
-    //     [
-    //         auroraTokenAddress,
-    //         startTime + parseInt(SCHEDULE_PERIOD as string)
-    //     ]
-    // )
-
     // Deploy Locked Staking Template
     const lockedStakingTemplate = await LockedStakingTemplate.deploy()
     console.log(`Deploy LockedStakingSubAccountImpalementation template @ ${lockedStakingTemplate.address}`)
     await new Promise(f => setTimeout(f, 3000))
+    // Deploy Locked Staking Factory
     const StakingFactory = await ethers.getContractFactory('StakingStrategyFactory')
     const stakingFactory = await upgrades.deployProxy(
         StakingFactory,
