dd14751d8196c28200aa297b76e01b7c32702433
lempire123 <61431140+lempire123@users.noreply.github.com>
2022-08-16 19:29:26 +0200

    comments

--- test/foundry/jetStaking.t.sol
+++ test/foundry/jetStaking.t.sol
@@ -6,21 +6,35 @@ import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 import "forge-std/Test.sol";
 
+/// @title JetStaking Foundry Tests
+/// @author Lance Henderson
+/// 
+/// @notice The following contract provides a suite of tests for JetStaking.sol
+/// contract. Further detail of what each tests does is provided above the actual
+/// test code. The tests must be run on a fork of aurora mainnet. given the use of 
+/// hardcoded values. To run the tests run 'forge test --rpc-url https://mainnet.aurora.dev/'
+/// In the future the code can be editted to not use hardcoded values.
+
 contract jetStakingTest is Test {
-    // Contract addresses
+    // JetStaking proxy contract address (ie. the one users interact with)
     JetStakingV1 jetStaking = JetStakingV1(0xccc2b1aD21666A5847A804a73a41F904C4a4A0Ec);
+    // JetStaking implementation contract address (ie. the one containing the logic)
     JetStakingV1 implementation = JetStakingV1(0x852F139Dd31D2cdc669470880700037Cb3790934);
-    // Roles
+    // AirdropRole has the power to stake on behalf of another user
     address airdropRole = address(0x88e21E0CeE6FbdAa1E7BBd2f7e25554144CDCE42); 
+    // ClaimRole has the power to claim rewards on behalf of another user
     address claimRole = address(0x88e21E0CeE6FbdAa1E7BBd2f7e25554144CDCE42);
+    // PauseRole has the power to pause the contract
     address pauseRole = address(0x88e21E0CeE6FbdAa1E7BBd2f7e25554144CDCE42);
+    // StreamManagerRole has the power to propose a stream
     address streamManagerRole = address(0x61c8f8f192C345424a0836d722892231CE7a47b8);
+    // DefaultAdminRole has the power to update the treasury address
     address defaultAdminRole = address(0x88e21E0CeE6FbdAa1E7BBd2f7e25554144CDCE42);
     // Aurora token
     IERC20 aurora = IERC20(0x8BEc47865aDe3B172A928df8f990Bc7f2A3b9f79);
-    // Reward tokens
+    // Reward tokens - will be initialized in the constructor by pulling them from jetStaking
     address[] rewardTokens;
-    // Accounts
+    // Accounts - useful for setting up potential scenarios between several users
     address user1 = address(1);
     address user2 = address(2);
     address user3 = address(3);
@@ -28,8 +42,10 @@ contract jetStakingTest is Test {
 
     // Code below is executed prior to every test
     function setUp() public {
-        deal(address(aurora), address(this), 10000 ether);
+        // 'Deal' - cheatcode provided by Foundry to set the balance of a token of a user
+        deal(address(aurora), address(this), 10000 ether); 
         uint256 length = jetStaking.getStreamsCount();
+        // Loop to pull tokens from jetStaking, and push into our array of reward tokens
         for(uint256 i; i < length; ++i) {
             (,address token,,,,,,,,,) = jetStaking.getStream(i);
             rewardTokens.push(token);
@@ -37,50 +53,56 @@ contract jetStakingTest is Test {
     } 
 
     /* ===================================
-            SIMPLE FUNCTIONALITY
+            TESTING BASIC FUNCTIONALITY
     ==================================== */
 
+    // @notice Steps in the following test:
+    // - Stakes 1000 ether
+    // - Waits 1 week
+    // - Moves rewards to pending
+    // - Waits 2 days + 1 second - Waiting is achieved by warping block.timestamp using vm.warp
+    // - Unstakes aurora
+    // - Check that balance withdrawn is greater than deposited
     function testStakeAndWithdraw() public {
         aurora.approve(address(jetStaking), 1000 ether);
         jetStaking.stake(1000 ether);
-        console.log(jetStaking.getUserShares(address(this)));
-        console.log(jetStaking.getUserTotalDeposit(address(this)));
         vm.warp(block.timestamp + 1 weeks);
         jetStaking.moveAllRewardsToPending();
         jetStaking.unstakeAll();
-        vm.warp(block.timestamp + 20 days);
+        vm.warp(block.timestamp + 2 days + 1);
         jetStaking.withdrawAll();
-        logUserBalances(address(this));
+        assert(aurora.balanceOf(address(this)) > 1000 ether);
+        // logUserBalances(address(this)); // Optional to check balances manually
     }
 
+    // @notice Steps in the following test:
+    // - Stakes 1000 ether
+    // - Waits 1 week
+    // - Moves rewards to pending
+    // - Waits 2 days + 1 second
+    // - Unstakes aurora + stream token 1
+    // - Check that balance withdrawn is greater than deposited (of both tokens)
     function testStakeAndBatchWithdraw() public {
         aurora.approve(address(jetStaking), 1000 ether);
         jetStaking.stake(1000 ether);
-        console.log(jetStaking.getUserShares(address(this)));
-        console.log(jetStaking.getUserTotalDeposit(address(this)));
         vm.warp(block.timestamp + 1 weeks);
         jetStaking.moveAllRewardsToPending();
         jetStaking.unstakeAll();
-        vm.warp(block.timestamp + 20 days);
+        vm.warp(block.timestamp + 2 days + 1);
         uint256[] memory streamId = new uint256[](2);
         streamId[0] = 0;
         streamId[1] = 1;
         jetStaking.batchWithdraw(streamId);
-        logUserBalances(address(this)); // Should only have balances for stream 0 & 1
+        assert(aurora.balanceOf(address(this)) > 1000 ether);
+        assert(IERC20(rewardTokens[1]).balanceOf(address(this)) > 0);
+        // logUserBalances(address(this)); // Should only have balances for stream 0 & 1
     }
 
-    function testStakeTwice() public {
-        aurora.approve(address(jetStaking), 20**21);
-        jetStaking.stake(10**21);
-        vm.warp(block.timestamp + 1 weeks);
-        jetStaking.stake(10**21);
-        jetStaking.moveAllRewardsToPending();
-        jetStaking.unstakeAll();
-        vm.warp(block.timestamp + 20 days);
-        jetStaking.withdrawAll();
-        logUserBalances(address(this)); // User loses all rewards when staking / unstaking
-    }
-
+    // @notice Steps in the following test:
+    // - Setup random scheduleTimes
+    // - Setup random scheduleRewards
+    // - Attempt to initialize the implementation contract
+    // - Expect a revert with the comment below
     function testCannotInitializeImplementation() public {
         uint256[] memory scheduleTimes = new uint256[](1);
         scheduleTimes[0] = 1;
@@ -104,24 +126,32 @@ contract jetStakingTest is Test {
                 EDGE CASES
     ==================================== */
 
+    // @notice Steps in the following test:
+    // - Stake 1 aurora
+    // - Immediately withdraw 1 aurora
+    // - Check that balance after is greater than before (due to rounding)
     function testStake1Aurora() public {
-        uint256 sharesPrice = jetStaking.getTotalAmountOfStakedAurora() * 1000 / jetStaking.totalAuroraShares();
-        emit log_named_uint("Aurora per share (divide by 1000)", sharesPrice);
-        deal(address(aurora), user3, 1);
-        vm.startPrank(user3);
+        // uint256 sharesPrice = jetStaking.getTotalAmountOfStakedAurora() * 1000 / jetStaking.totalAuroraShares();
         aurora.approve(address(jetStaking), 1);
         jetStaking.stake(1);
-        emit log_named_uint("Shares received", jetStaking.getUserShares(user3));
         jetStaking.unstakeAll();
         vm.warp(block.timestamp + 2 days + 1);
         jetStaking.withdrawAll();
-        emit log_named_uint("Aurora received from 1 aurora", aurora.balanceOf(user3));
+        assert(aurora.balanceOf(address(this)) > 1);
     }
 
+    // @notice Steps in the following test:
+    // - Deal 10000 aurora tokens to user1
+    // - Impersonate user1 using vm.startPrank cheatcode
+    // - Rather than staking all 10000 tokens at once, we stake iteratively in single quantities
+    // - Immediately unstake
+    // - Assert that balance after is greater than 10000
+    // - Output profit generate (balanceAfter - balanceBefore)
     function testGameSystem() public {
-        uint256 balanceBefore = aurora.balanceOf(user1);
         deal(address(aurora), user1, 10000);
+        // Useful Foundry cheatcode to impersonate an address (user1)
         vm.startPrank(user1);
+        uint256 balanceBefore = aurora.balanceOf(user1);
         aurora.approve(address(jetStaking), 10000);
         for(uint256 i; i < 10000; i++) {
             jetStaking.stake(1);
@@ -133,21 +163,12 @@ contract jetStakingTest is Test {
         emit log_named_uint("Aurora profit = ", balanceAfter - balanceBefore);
     }
 
-    function testRealisticExploit() public {
-        uint256 balanceBefore = aurora.balanceOf(user1);
-        deal(address(aurora), user1, 500);
-        vm.startPrank(user1);
-        aurora.approve(address(jetStaking), 500);
-        for(uint256 i; i < 500; i++) {
-            jetStaking.stake(1);
-        }
-        jetStaking.unstakeAll();
-        vm.warp(block.timestamp + 2 days + 1);
-        jetStaking.withdrawAll();
-        uint256 balanceAfter = aurora.balanceOf(user1);
-        emit log_named_uint("Aurora profit = ", balanceAfter - balanceBefore);
-    }
-
+    // @notice Steps in the following test:
+    // - Stake 10 aurora for user1 using the stakeFor function
+    // - Stake 10 aurora for user2
+    // - Wait 10 weeks
+    // - Move rewards to pending and unstake for both users
+    // - Make sure that the order in which users unstake doesnt affect their rewards
     function testWithdrawalOrder() public {
         stakeFor(user1, 10 ether);
         stakeFor(user2, 10 ether);
@@ -165,10 +186,17 @@ contract jetStakingTest is Test {
         jetStaking.withdrawAll();
         vm.prank(user2);
         jetStaking.withdrawAll();
+        assert(aurora.balanceOf(user1) == aurora.balanceOf(user2));
         logUserBalances(user1);
         logUserBalances(user2);
     }
 
+    // @notice Steps in the following test:
+    // - Stake 2 aurora on behalf of user2
+    // - Wait 10 weeks (user2 should accumulate rewards)
+    // - Whale (user3) stakes 10^12 auroras
+    // - User2 unstakes and withdraws
+    // - User2 should not be robbed of rewards by whale sniper
     function testWhaleSniper() public { 
         stakeFor(user2, 10 ether);
         vm.warp(block.timestamp + 10 weeks);
@@ -178,9 +206,18 @@ contract jetStakingTest is Test {
         jetStaking.unstakeAll();
         vm.warp(block.timestamp + 2 days + 1);
         jetStaking.withdrawAll();
+        assert(aurora.balanceOf(user2) > 0);
+        assert(IERC20(rewardTokens[1]).balanceOf(user2) > 0);
+        assert(IERC20(rewardTokens[2]).balanceOf(user2) > 0);
+        assert(IERC20(rewardTokens[3]).balanceOf(user2) > 0);
         logUserBalances(user2); 
     }
 
+    // @notice Steps in the following test:
+    // - Stake 2 aurora on behalf of user1
+    // - Move timestamp 1 week back
+    // - Try moving rewards to pending
+    // - Expect a revert with the message below
     function testTimeTravel() public {
         stakeFor(user1, 10 ether);
         vm.warp(block.timestamp - 1 weeks);
@@ -194,6 +231,13 @@ contract jetStakingTest is Test {
              JetStaking view
     ==================================== */
 
+    // @notice This function aims at getting general data from the staking contract:
+    // - Number of active streams
+    // - Treasury balance of each stream
+    // - How much aurora the owner of each stream has available to claim
+    // - RPS (rewards per share)
+    // - Latest rewards per share
+    // - Reward Amount
     function testProbeContractStatus() public {
         emit log("General info");
         emit log_named_uint("Stream count", jetStaking.getStreamsCount());
@@ -207,68 +251,14 @@ contract jetStakingTest is Test {
             }  
             emit log_named_uint("Reward amount", jetStaking.getRewardsAmount(i, block.timestamp - 1) / 1 ether);
         }
-
-    }
-
-    /* ===================================
-             JetStaking Roles
-    ==================================== */
-    
-    function testAirdropRole() public {
-        address[] memory accounts = new address[](2);
-        accounts[0] = user1;
-        accounts[1] = user2;
-        uint256[] memory amounts = new uint256[](2);
-        amounts[0] = 10 ether;
-        amounts[1] = 10 ether;
-        uint256 batchAmount = 20 ether;
-        deal(address(aurora), airdropRole, batchAmount);
-        vm.startPrank(airdropRole);
-        aurora.approve(address(jetStaking), batchAmount);
-        jetStaking.stakeOnBehalfOfOtherUsers(accounts, amounts, batchAmount);
-        assert(jetStaking.getUserTotalDeposit(user1) + jetStaking.getUserTotalDeposit(user2) == 20 ether);
-    }
-
-    function testStreamManagerRole() public {
-        vm.startPrank(streamManagerRole);
-        address newToken = address(8);
-        uint256[] memory scheduleTimes = new uint256[](2);
-        scheduleTimes[0] = block.timestamp + 1000;
-        scheduleTimes[1] = block.timestamp + 2000;
-        uint256[] memory scheduleRewards = new uint256[](2);
-        scheduleRewards[0] = 1000;
-        scheduleRewards[1] = 0;
-         // Would recommend support for the token to be added "on demand"
-        vm.expectRevert("INVALID_SUPPORTED_TOKEN_ADDRESS"); 
-        jetStaking.proposeStream(
-            user1, 
-            newToken, 
-            100, 
-            1000, 
-            100, 
-            scheduleTimes, 
-            scheduleRewards, 
-            1 days
-            );
-    }
-
-    function testClaimRole() public {
-        stakeFor(user1, 10 ether);
-        vm.warp(block.timestamp + 1 weeks);
-        vm.startPrank(claimRole);
-        jetStaking.claimAllOnBehalfOfAnotherUser(user1);
-    }
-
-    function testUpdateTreasury() public {
-        vm.startPrank(defaultAdminRole);
-        jetStaking.adminPause(1);
-        jetStaking.updateTreasury(address(10));
     }
 
     /* ===================================
                     HELPER
     ==================================== */
 
+    // @notice Helper function to log user balances of each stream token
+    // @param user Address of user to log balances of
     function logUserBalances(address user) public {
         emit log_named_address("Balances for", user);
         uint256 length = rewardTokens.length;
@@ -277,6 +267,9 @@ contract jetStakingTest is Test {
         }
     } 
 
+    // @notice Helper function to stake aurora tokens 
+    // @param account Address of account to stake for
+    // @param amount Amount of aurora to stake
     function stakeFor(address account, uint256 amount) public {
         deal(address(aurora), account, amount);
         vm.startPrank(account);
@@ -284,6 +277,4 @@ contract jetStakingTest is Test {
         jetStaking.stake(amount);
         vm.stopPrank();
     }
-
-      
 }
---
+++ test/foundry/jetStakingRoles.t.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import "./jetStaking.t.sol";
+
+contract jetStakingRoles is jetStakingTest {
+    // @notice Steps in the following test:
+    // - Create an array of account
+    // - Create an array of amount (uints)
+    // - Deal 20 aurora to airdrop role address
+    // - Impersonate airdrop role address
+    // - Use airdrop role to stake 10 aurora on behalf of user1 and user2 (10 each)
+    // - Assert that the recorded deposits of user1 and user2 in jetStaking is 10 aurora
+    function testAirdropRole() public {
+        address[] memory accounts = new address[](2);
+        accounts[0] = user1;
+        accounts[1] = user2;
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = 10 ether;
+        amounts[1] = 10 ether;
+        uint256 batchAmount = 20 ether;
+        deal(address(aurora), airdropRole, batchAmount);
+        vm.startPrank(airdropRole);
+        aurora.approve(address(jetStaking), batchAmount);
+        jetStaking.stakeOnBehalfOfOtherUsers(accounts, amounts, batchAmount);
+        assert(jetStaking.getUserTotalDeposit(user1) + jetStaking.getUserTotalDeposit(user2) == 20 ether);
+    }
+
+    // @notice Steps in the following test:
+    // - Impersonate streamManager address
+    // - Attempt to propose a stream
+    // - Should revert given the token is not supported by the treasury
+    function testStreamManagerRole() public {
+        vm.startPrank(streamManagerRole);
+        address newToken = address(8);
+        uint256[] memory scheduleTimes = new uint256[](2);
+        scheduleTimes[0] = block.timestamp + 1000;
+        scheduleTimes[1] = block.timestamp + 2000;
+        uint256[] memory scheduleRewards = new uint256[](2);
+        scheduleRewards[0] = 1000;
+        scheduleRewards[1] = 0;
+         // Would recommend support for the token to be added "on demand"
+        vm.expectRevert("INVALID_SUPPORTED_TOKEN_ADDRESS"); 
+        jetStaking.proposeStream(
+            user1, 
+            newToken, 
+            100, 
+            1000, 
+            100, 
+            scheduleTimes, 
+            scheduleRewards, 
+            1 days
+            );
+    }
+
+    // @notice Steps in the following test:
+    // - Stake 10 aurora for user1
+    // - Impersonate claimRole address
+    // - Claim rewards on behalf of user1
+    // - Try claiming using a random address (user2)
+    // - Expect the revert message below
+    function testClaimRole() public {
+        stakeFor(user1, 10 ether);
+        vm.warp(block.timestamp + 1 weeks);
+        vm.startPrank(claimRole);
+        jetStaking.claimAllOnBehalfOfAnotherUser(user1);
+        vm.warp(block.timestamp + 1 weeks);
+        vm.stopPrank();
+        vm.startPrank(user2);
+        vm.expectRevert("AccessControl: account 0x0000000000000000000000000000000000000002 is missing role 0xf7db13299c8a9e501861f04c20f69a2444829a36a363cfad4b58864709c75560");
+        jetStaking.claimAllOnBehalfOfAnotherUser(user1);
+    }
+
+    // @notice Steps in the following test:
+    // - Impersonate adminRole address
+    // - Pause the contract
+    // - Update the Treasury
+    // - Attempt the same with a non-admin account
+    // - Expect revert
+    function testUpdateTreasury() public {
+        vm.startPrank(defaultAdminRole);
+        jetStaking.adminPause(1);
+        jetStaking.updateTreasury(address(10));
+        vm.stopPrank();
+        vm.startPrank(user1);
+        vm.expectRevert("AccessControl: account 0x0000000000000000000000000000000000000001 is missing role 0x0000000000000000000000000000000000000000000000000000000000000000");
+        jetStaking.updateTreasury(address(10));
+    }
+}
