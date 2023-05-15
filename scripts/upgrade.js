75588afb89b3f679bcd921b0b0eddcb451d94153
ahmed <ahmed.ali@aurora.dev>
2023-05-15 13:28:05 +0200

    minor fix in the upgrade.js

--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -1,7 +1,7 @@
 const { ethers, upgrades } = require("hardhat");
 //the address of the deployed proxy
 const PROXY = "0x98bE4f05CAAe308b6B3f6f57B705a43A716f511e"; // replace me
-const CONTRACT_NAME = 'JetStakingV1ChangeInStorage'
+const CONTRACT_NAME = 'JetStakingV2' // replace me
 
 async function main() {
     const {owner} = await getNamedAccounts();
