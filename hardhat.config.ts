93cbbda8faf50b9071f0274fccd260c3a7108ae2
idoh-spherex <ido.h@sphere-x.xyz>
2023-10-25 20:00:58 +0300

    Fixed formatting in hardhat config.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,38 +1,48 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 
-import "@typechain/hardhat";
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-waffle";
-import "@openzeppelin/hardhat-upgrades";
-import "hardhat-deploy";
-import "solidity-coverage";
-import "hardhat-docgen";
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-waffle';
+import '@openzeppelin/hardhat-upgrades';
+import 'hardhat-deploy';
+import 'solidity-coverage';
+import 'hardhat-docgen';
 import "@nomiclabs/hardhat-etherscan";
 import "hardhat-contract-sizer";
 
 dotenv.config();
 
-const { INFURA_KEY, MNEMONIC, ETHERSCAN_API_KEY, PRIVATE_KEY, AURORA_API_KEY } = process.env;
+const {
+  INFURA_KEY,
+  MNEMONIC,
+  ETHERSCAN_API_KEY,
+  PRIVATE_KEY,
+  AURORA_API_KEY,
+} = process.env;
 
-const accountsTestnet = PRIVATE_KEY ? [PRIVATE_KEY] : { mnemonic: MNEMONIC };
+const accountsTestnet = PRIVATE_KEY
+  ? [PRIVATE_KEY]
+  : { mnemonic: MNEMONIC };
 
-const accountsMainnet = PRIVATE_KEY ? [PRIVATE_KEY] : { mnemonic: MNEMONIC };
+const accountsMainnet = PRIVATE_KEY
+  ? [PRIVATE_KEY]
+  : { mnemonic: MNEMONIC };
 
 module.exports = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: 'hardhat',
   solidity: {
     version: "0.8.10",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 1,
+        runs: 1
       },
       outputSelection: {
         "*": {
           "*": ["storageLayout"],
         },
-      },
-    },
+      }
+    }
   },
   contractSizer: {
     disambiguatePaths: false,
@@ -58,22 +68,22 @@ module.exports = {
     },
     auroraTestnet: {
       url: `https://testnet.aurora.dev/${AURORA_API_KEY}`,
-      accounts: accountsTestnet,
+      accounts: accountsTestnet
     },
     aurora: {
       url: `https://mainnet.aurora.dev/${AURORA_API_KEY}`,
-      accounts: accountsMainnet,
-    },
+      accounts: accountsMainnet
+    }
   },
   etherscan: {
     // Your API key for Aurorascan
     // Obtain one at https://aurorascan.dev/
     apiKey: {
-      aurora: ETHERSCAN_API_KEY,
+      aurora: ETHERSCAN_API_KEY
     },
   },
   docgen: {
-    path: "./docs/contracts",
+    path: './docs/contracts',
     clear: true,
     runOnCompile: true,
   },
